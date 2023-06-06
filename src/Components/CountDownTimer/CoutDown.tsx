daf92ee63ae64ea0d5a036107d308da8bf93b0c7
Hoan Truong <hoantruong@Hoans-MacBook-Air.local>
2023-06-06 23:03:32 +0700

    update readme && reNew coming soon modal

--- src/Components/CountDownTimer/CoutDown.tsx
+++ src/Components/CountDownTimer/CoutDown.tsx
@@ -8,14 +8,14 @@ interface CountDownProps {
     setIsVisible: any
 }
 
-export default function CountDown({setIsVisible}:CountDownProps) {
+export default function CountDown({ setIsVisible }: CountDownProps) {
     const TIME_LIMIT = 300 // 5 minutes = 300s
 
     const [time, setTime] = useState(() => {
         const countTime = getCountTime()
         if (countTime > 0) return countTime
         return TIME_LIMIT
-        // if (countTime) 
+        // if (countTime)
         //     setTime(countTime)
     })
 
@@ -45,16 +45,16 @@ export default function CountDown({setIsVisible}:CountDownProps) {
                 setTime((prev) => prev - 1)
             } else {
                 clearInterval(timeID)
-                window.localStorage.setItem('countTime','-1')
+                window.localStorage.setItem('countTime', '-1')
                 setIsVisible(false)
             }
         }, 1000)
-        window.addEventListener("beforeunload", onUnload);
+        window.addEventListener('beforeunload', onUnload)
         return () => {
             clearInterval(timeID)
-            window.removeEventListener("beforeunload", onUnload);
+            window.removeEventListener('beforeunload', onUnload)
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [time])
 
     return (
@@ -67,12 +67,23 @@ export default function CountDown({setIsVisible}:CountDownProps) {
                         transform: 'translate(0%, -50%)'
                     }}
                 >
-                    <div className='shadow-xl p-6 md:w-[400px] w-[300px] bg-white rounded-xl'>
+                    <div className='shadow-xl p-6 md:w-[400px] w-[300px] bg-gradient-blue rounded-xl'>
                         <div className='flex flex-col justify-center items-center gap-2'>
-                            <Lottie options={defaultOptions} height={120} width={120} isStopped={false} isPaused={false} />
-                            <p>Please waiting for {formatTime(time)}</p>
-                        </div>
+                            <div className='bg-white rounded-full'>
+                                <Lottie
+                                    options={defaultOptions}
+                                    height={120}
+                                    width={120}
+                                    isStopped={false}
+                                    isPaused={false}
+                                />
+                            </div>
+                            <h3 className='md:text-5xl text-white'>Coming soon</h3>
 
+                            <span className='text-xl font-bold text-slate-200'> {formatTime(time)}</span>
+
+                            <span className='text-slate-100 text-sm'>{`We're currently working hard on this page`}</span>
+                        </div>
                     </div>
                 </div>
             </div>
